f1ba14f2a6183437ba34199e369477f720d96b8c
Nishant Bhardwaz <nishant.bhardwaz23@gmail.com>
2019-11-25 23:00:16 +0530

    s

---
+++ premierLeague-portal/src/app/modals/OEPLTeam.ts
@@ -0,0 +1,12 @@
+import { OEPLMember } from './OEPLMember';
+
+
+export class OEPLTeam {
+    id: number;
+    teamName: string;
+    captain: OEPLMember = new OEPLMember();
+    members: OEPLMember[] = [];
+    amount: number;
+    imageUrl: string;
+    maxBidAmount: number;
+}
